7a37adf926355058c8c24aec07f6ac0d64ab98cd
Alexey Smyk <alexey@RAYDOMAIN.COM>
2026-06-02 13:35:59 +0400

    локи

--- index.js
+++ index.js
@@ -163,6 +163,7 @@ app.get('/admin', basicAuth, (req, res) => {
 });
 
 app.post('/webhook/mis', async (req, res) => {
+  console.log('RAW BODY FULL:', req.body); // 🔥 ВОТ ЭТО
    const secret = req.query.secret; // 🔥 ВОТ ЭТО ДОБАВИТЬ
   if (secret !== process.env.MIS_WEBHOOK_SECRET) {
   return res.status(403).send('Forbidden');
