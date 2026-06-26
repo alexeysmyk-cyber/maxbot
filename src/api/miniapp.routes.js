60a91c96ed23ba996c4963a3de3dd954daaf6d3b
Alexey Smyk <alexey.smyk@gmail.com>
2026-06-26 16:21:00 +0300

    addr

--- src/api/miniapp.routes.js
+++ src/api/miniapp.routes.js
@@ -420,10 +420,9 @@ router.post("/appointments/day", async (req, res) => {
 
       date_from: `${date} 00:00`,
 
-      date_to: `${date} 23:59`,
+      date_to: `${date} 23:59`
 
-      status_id: "1,2,3"
-
+      
     };
 
     console.log("➡️ GET APPOINTMENTS:", body);
